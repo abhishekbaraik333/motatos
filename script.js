4b3965625d8a24552c8ec8daa3f489c2f88eb8f0
abhishekbaraik333 <abhitamangme@gmail.com>
2024-09-19 14:14:03 +0530

    added akk cards

--- script.js
+++ script.js
@@ -12,6 +12,7 @@
   });
   
 
+  // Dialogs
   const openDialogBtn1 = document.getElementById('openDialog1');
   const closeDialogBtn1 = document.getElementById('closeDialog1');
   const dialog1 = document.getElementById('myDialog1');
@@ -63,6 +64,60 @@
       overlay2.style.display = 'none';
       document.body.classList.remove('show-dialog');
   });
+
+  const openDialogBtn3 = document.getElementById('openDialog3');
+  const closeDialogBtn3 = document.getElementById('closeDialog3');
+  const dialog3 = document.getElementById('myDialog3');
+  const overlay3 = document.getElementById('overlay3');
+
+  // Open the dialog
+  openDialogBtn3.addEventListener('click', () => {
+      dialog3.style.display = 'block';
+      overlay3.style.display = 'block';
+      document.body.classList.add('show-dialog');
+  });
+
+  // Close the dialog
+  closeDialogBtn3.addEventListener('click', () => {
+      dialog3.style.display = 'none';
+      overlay3.style.display = 'none';
+      document.body.classList.remove('show-dialog');
+  });
+
+  // Close the dialog when clicking on the overlay
+  overlay3.addEventListener('click', () => {
+      dialog3.style.display = 'none';
+      overlay3.style.display = 'none';
+      document.body.classList.remove('show-dialog');
+  });
+
+  const openDialogBtn4 = document.getElementById('openDialog4');
+  const closeDialogBtn4 = document.getElementById('closeDialog4');
+  const dialog4 = document.getElementById('myDialog4');
+  const overlay4 = document.getElementById('overlay4');
+
+  // Open the dialog
+  openDialogBtn4.addEventListener('click', () => {
+      dialog4.style.display = 'block';
+      overlay4.style.display = 'block';
+      document.body.classList.add('show-dialog');
+  });
+
+  // Close the dialog
+  closeDialogBtn4.addEventListener('click', () => {
+      dialog4.style.display = 'none';
+      overlay4.style.display = 'none';
+      document.body.classList.remove('show-dialog');
+  });
+
+  // Close the dialog when clicking on the overlay
+  overlay4.addEventListener('click', () => {
+      dialog4.style.display = 'none';
+      overlay4.style.display = 'none';
+      document.body.classList.remove('show-dialog');
+  });
+
+  // Dialogs end
 
   document.querySelector(".hide-btn").addEventListener("click",()=>{
     document.querySelector('.banner').style.display = 'none';
